e34b0b2693d11f6c8b444ba985369a6618f82d39
archanaweb <thaarchana7jan@gmail.com>
2023-02-18 22:55:07 +0530

    file order update

---
+++ src/Sdata.jsx
@@ -0,0 +1,41 @@
+const Sdata = [
+    {
+        imgsrc: "https://img.republicworld.com/republic-prod/stories/promolarge/xhdpi/gbepvjettqttzqlz_1593258788.jpeg",
+        title: "A Netflix Original Series",
+        sname: "DARK",
+        link: "https://www.netflix.com/in/title/80100172"
+    },
+    {
+        imgsrc: "https://www.heavenofhorror.com/wp-content/uploads/2020/04/Extracurricular-netflix-review.jpg",
+        title: "A Netflix Original Series",
+        sname: "Extra Curricular",
+        link: "https://www.netflix.com/in/title/80100172"
+    },
+    {
+        imgsrc: "https://www.nme.com/wp-content/uploads/2022/05/Stranger_Things_Cast.jpg",
+        title: "A Netflix Original Series",
+        sname: "Sranger Things",
+        link: "https://www.netflix.com/in/title/80100172"
+    },
+    {
+        imgsrc: "https://www.heavenofhorror.com/wp-content/uploads/2020/04/Extracurricular-netflix-review.jpg",
+        title: "A Netflix Original Series",
+        sname: "Extra Curricular",
+        link: "https://www.netflix.com/in/title/80100172"
+    },
+    {
+        imgsrc: "https://www.nme.com/wp-content/uploads/2022/05/Stranger_Things_Cast.jpg",
+        title: "A Netflix Original Series",
+        sname: "Sranger Things",
+        link: "https://www.netflix.com/in/title/80100172"
+    },
+
+    {
+        imgsrc: "https://www.nme.com/wp-content/uploads/2022/05/Stranger_Things_Cast.jpg",
+        title: "A Netflix Original Series",
+        sname: "Better Together",
+        link: "https://www.netflix.com/in/title/80100172"
+    },
+]
+
+export default Sdata;
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Cards from './Scard';
+import './index.css';
+import './media.css';
+import Sdata from './Sdata';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ <>
+  <h1 className='heading_style'>List of top 5 Netflix Series in 2020</h1>
+
+{Sdata.map((val) =>{
+  return(
+    <Cards imgsrc={val.imgsrc} 
+    title={val.title}
+    sname={val.sname} />
+    );
+
+})
+}
+ </>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
